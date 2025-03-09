3a9492ad4ae88004b54668943178af6c6827c21a
Sajith Dilhan <sajithdilhan@gmail.com>
2025-03-09 12:08:28 +0800

    Change IAM auth modeInitial commit

--- SecretManager_demo/Program.cs
+++ SecretManager_demo/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+
+var awsCredentials = new BasicAWSCredentials("", "");
+IAmazonSecretsManager secretsManager = new AmazonSecretsManagerClient(awsCredentials, RegionEndpoint.GetBySystemName("ap-southeast-1"));
+
+GetSecretValueRequest svRequest = new GetSecretValueRequest()
+{
+    SecretId = "test_pw"
+};
+
+CancellationTokenSource cts = new CancellationTokenSource(2000);
+
+try
+{
+    var res = await secretsManager.GetSecretValueAsync(svRequest, cts.Token);
+    if (res != null)
+    {
+        Console.WriteLine(res.SecretString);
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
+
